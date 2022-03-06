dd1a6970d6ea5d7adf82b068599b6184e6e67395
Jacob Meredith <jacobmeredith@hotmail.co.uk>
2022-03-06 14:02:43 +0000

    refactor(posts): add extra posts and update date format

--- app/utils/posts.ts
+++ app/utils/posts.ts
@@ -1,6 +1,18 @@
 import * as post from "../routes/blog/post.mdx";
+import * as post2 from "../routes/blog/post2.mdx";
+import * as post3 from "../routes/blog/post3.mdx";
 
-export const postFromModule = (mod: any) => {
+import { parseISO } from "date-fns";
+
+export interface IPost {
+  slug: string;
+  title: string;
+  category: string;
+  createdDate: string;
+  updatedDate: string;
+}
+
+export const postFromModule = (mod: any): IPost => {
   return {
     slug: mod.filename.replace(/\.mdx?$/, ""),
     ...mod.attributes,
@@ -8,4 +20,16 @@ export const postFromModule = (mod: any) => {
 };
 
 // Make sure to add each post to this list, need to come up with a better way to do this
-export const allPosts = [postFromModule(post)];
+export const allPosts: IPost[] = [
+  postFromModule(post),
+  postFromModule(post2),
+  postFromModule(post3),
+];
+export const takeLatestPosts = (n: number = 3, skip: number = 0) => {
+  return allPosts
+    .sort(
+      (a, b) =>
+        parseISO(b.createdDate).getTime() - parseISO(a.updatedDate).getTime()
+    )
+    .slice(skip, n);
+};
