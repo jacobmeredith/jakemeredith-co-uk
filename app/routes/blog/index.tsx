f3d81e48ae20307158ca222cae7b17194edb89f6
Jacob Meredith <jacobmeredith@hotmail.co.uk>
2022-03-06 11:04:17 +0000

    feature(blog): add category list to blog page

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -12,8 +12,15 @@ export const loader: LoaderFunction = async ({ request }) => {
   let url = new URL(request.url);
   let category = url.searchParams.get("c");
   let categoryArray = category?.split(",");
+  let categories = allPosts.reduce((acc, post) => {
+    if (!acc.includes(post.category)) {
+      acc.push(post.category);
+    }
+    return acc;
+  }, []);
 
   return {
+    categories,
     posts: category
       ? allPosts.filter((post) =>
           categoryArray?.includes(post.category.toLowerCase())
@@ -23,7 +30,13 @@ export const loader: LoaderFunction = async ({ request }) => {
 };
 
 export default function Blog() {
-  const { posts } = useLoaderData();
+  const { posts, categories } = useLoaderData();
+
+  const categoriesMap = categories.map((category: string) => (
+    <Link key={category} to={`/blog/?c=${category.toLowerCase()}`}>
+      {category}
+    </Link>
+  ));
 
   const postsMap = posts.map((post: any) => (
     <div key={post.slug}>
@@ -31,5 +44,10 @@ export default function Blog() {
     </div>
   ));
 
-  return <div>{postsMap}</div>;
+  return (
+    <React.Fragment>
+      {categoriesMap}
+      {postsMap}
+    </React.Fragment>
+  );
 }
