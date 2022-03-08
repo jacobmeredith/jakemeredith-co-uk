b4ff9676f6445732b57f82ce6cbb33c54d7824ee
Jacob Meredith <jacobmeredith@hotmail.co.uk>
2022-03-08 22:49:55 +0000

    chore(page): home page header text

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -22,8 +22,7 @@ export default function Index() {
     <React.Fragment>
       <header className="container px-8 py-12 mx-auto md:py-24 md:px-0">
         <h2 className="w-full text-2xl leading-relaxed text-gray-100 md:w-1/2 md:text-4xl">
-          Hi I'm Jake, I'm a software engineer from the UK that works with
-          javascript/typescript.
+          Hi I'm Jake, and I am a selft taught software engineer from the UK
         </h2>
       </header>
       <RecentPosts title="Recent articles" posts={recentPosts} />
